93229eefe6ee823faf00fdeeaeb75659fc012faf
qws <13374@qq.com>
2023-04-13 17:45:15 +0800

    2

--- k_anonymous.py
+++ k_anonymous.py
@@ -8,18 +8,3 @@ def isKAnonymized(df, k):
         if rows < k:
             return False
     return True
-
-
-# if __name__ == 'main':
-plt.style.use('seaborn-v0_8-whitegrid')
-raw_data = {
-        'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
-        'last_name': ['Miller', 'Jacobson', 'Ali', 'Milner', 'Cooze'],
-        'age': [42, 52, 36, 24, 73],
-        'preTestScore': [4, 24, 31, 2, 3],
-        'postTestScore': [25, 94, 57, 62, 70]}
-    # df = pd.DataFrame(raw_data, columns = ['first_name', 'last_name', 'age', 'preTestScore', 'postTestScore'])
-df = pd.DataFrame(raw_data, columns=['age', 'preTestScore', 'postTestScore'])
-df
-#isKAnonymized(df,2)
-
---
+++ setting/data_conf.py
@@ -0,0 +1,21 @@
+# 属性标签
+attributeLabels = ["age",  # 0年龄            int64
+                       "workclass",  # 1工作类型        object
+                       "fnlwgt",  # 2人口特征权重    int64
+                       "education",  # 3学历            object
+                       "education_num",  # 4受教育时间      int64
+                       "marital_status",  # 5婚姻状态        object
+                       "occupation",  # 6职业            object
+                       "relationship",  # 7关系            object
+                       "race",  # 8种族            object
+                       "sex",  # 9性别            object
+                       "capital_gain",  # 10资本收益        int64
+                       "capital_loss",  # 11资本损失        int64
+                       "hours_per_week",  # 12每周工作小时数  int64
+                       "native_country",  # 13原籍            object
+                       "wage_class"]  # 14收入类别        object
+# 准标识符定义
+quasi_identifier_list = []
+quasi_identifier_DGH_list = []
+quasi_identifier_VGH_list = []
+quasi_identifier_height_list = []
